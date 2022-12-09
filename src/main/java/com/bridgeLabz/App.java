b19c07001c8ce4734da31296cd1c842d41955a4b
saurav4986 <saurav.raj4986@gmail.com>
2022-12-09 13:02:22 +0530

    Added: Welcome message

---
+++ src/main/java/com/bridgeLabz/App.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+/**
+ * Welcome to Mood-Analyzer!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Mood-Analyzer!" );
+    }
+}
